bc057e9fae17512fbbcced55c930916c9657eaa0
Qasimtalkin <abulqasim110@gmail.com>
2020-05-26 11:20:03 -0400

    Updates

---
+++ precache-manifest.7684731fdb29c65f27cbb5c0038761c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03950c1d7949912382bb2a83631ec7b",
+    "url": "/COVID19-Global-Visulaizer-React-JS/index.html"
+  },
+  {
+    "revision": "615f65affb8e347cc91c",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/css/main.7ab072ca.chunk.css"
+  },
+  {
+    "revision": "1a563568c4d964ef6de1",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/js/2.16cc8b0a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/js/2.16cc8b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615f65affb8e347cc91c",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/js/main.9df1389e.chunk.js"
+  },
+  {
+    "revision": "e6c8f8d6874a50f0ed7d",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/js/runtime-main.339d6332.js"
+  },
+  {
+    "revision": "6b43a751aa4d23a657bfa36479f61f41",
+    "url": "/COVID19-Global-Visulaizer-React-JS/static/media/logo.6b43a751.png"
+  }
+]);
